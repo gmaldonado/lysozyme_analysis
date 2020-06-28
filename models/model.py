b325cbb6c8d4f44327f8ed733a26122fdba83d6f
Gonzalo Maldonado <gemaldonadoleiva@gmail.com>
2020-06-28 18:29:34 +0100

    adding verbose mode for k-cv

--- models/model.py
+++ models/model.py
@@ -2,8 +2,8 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import roc_curve
 from models.constant import FPR,TPR,DUMMY_CLASSIFIER
 from matplotlib import pyplot
-from sklearn.model_selection import RepeatedKFold
-
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import classification_report,confusion_matrix
 
 class Model:
 
@@ -11,13 +11,27 @@ class Model:
         self.name = name
         self.instance = instance
         self.parameters = parameters
-        self.folds = []
 
-    def train(self,X,Y,verbose=0,splits=5,repeats=1):
-        self.instance = GridSearchCV(self.instance,self.parameters,refit=True,verbose=verbose,cv=RepeatedKFold(n_splits=splits,n_repeats=repeats))
+    def train(self,X,Y,verbose=False,splits=5):
+        self.instance = GridSearchCV(self.instance,self.parameters,refit=True,cv=StratifiedKFold(n_splits=splits))
+
+        if verbose:
+            print("Starting verbose mode")
+            #TODO check the object references and this implementation in general
+            temporary = Model(self.name,self.instance,self.parameters)
+            i = 1
+            for train,test in temporary.instance.cv.split(X,Y):
+                print("Split {}: \n Train: {} Test: {} ".format(i, train,test))
+                temporary.instance = temporary.instance.fit(X.iloc[train],Y.iloc[train])
+                pred = temporary.instance.predict(X.iloc[test])
+                print(confusion_matrix(Y.iloc[test],pred))
+                print(classification_report(Y.iloc[test], pred))
+                temporary.draw_roc_curve(X.iloc[test],Y.iloc[test])
+                i+=1
+
         self.instance = self.instance.fit(X,Y)
-        for train,test in self.instance.cv.split(X):
-            self.folds.append((train,test))
+
+
 
     def predict(self,dataset):
         return self.instance.predict(dataset)
